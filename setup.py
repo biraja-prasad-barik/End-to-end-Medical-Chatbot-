7cb159da8cd321a04e12cb13ad538f2b20315e35
biraja prasad barik <kbarik153@gmail.com>
2025-10-06 12:20:12 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Biraja prasad Barik',
+    author_email='kbarik153@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
